dc123e36b80cefad58865eb74854d4e8701b2954
Zahar778 <zax.kobra@gmail.com>
2023-12-26 21:08:13 +0200

    work

--- back/src/route/post.js
+++ back/src/route/post.js
@@ -11,45 +11,104 @@ const { Post } = require('../class/post')
 router.post('/post-create', function(req, res) {
     try {
         const { username, text, postId } = req.body
-
         if (!username || !text) {
             return res.status(400).json({
                 message:
-                'Потрiбно передати вci данi для створення поста',
+                    'Потрібно передати всі дані для створення поста',
             })
         }
-
         let post = null
-
         console.log(postId, 'postId')
-
         if(postId) {
             post = Post.getById(Number(postId))
             console.log('post', post)
-
             if(!post) {
                 return res.status(400).json({
-                    message:
-                    'Пост з таким Id не iснуэ',
+                    message: 'Пост з таким ID не існує',
+                })
+            }
+        }
+        const newPost = Post.create(username, text, post)
+        return res.status(200).json({
+            post: {
+                id: newPost.id,
+                text: newPost.text,
+                username: newPost.username,
+                date: newPost.date,
+            },
+        })
+    } catch (e) {
+        return res.status(400).json({
+            message: e.message,
+        })
+    }
+})
+
+router.get ('/post-list', function(req,res) {
+    try{
+        const list = Post.getList()
+
+        if(list.length === 0) {
+            return res.status(200).json({
+                list: [],
             })
         }
-    } 
-    const replyPost = Post.create(usetname, text, post)
-
-    return res.status(400).json({
-        post: {
-            id: replyPost.id,
-            text: replyPost.text,
-            username: replyPost.username,
-            data: replyPost.data,
-        },
-    })
-} catch (e) {
-    return res.status(400).json({
+        return res.status(200).json({
+            list: list.map(({id,username,text,data}) =>({
+                id,
+                username,
+                text,
+                data,
+            })),
+        })
+    }catch (e) {
+        return res.status(400).json({
         message: e.message,
     })
 }
 })
 
+router.get('/post-item', function (req, res) {
+    try{
+        const {id} = req.query
+
+        if(!id) {
+            return res.status(400).json({
+                message: "Потрiбно передти ID поста",
+            })
+        }
+        const post = Post.getById(Number(id))
+
+        if(!post) {
+            return res.status(400).json({
+                message: " Пост з таким Id не icнуэ",
+            })
+        }
+        return res.status(200).json({
+            post: {
+                id: post.id,
+                text: post.text,
+                username: post.username,
+                date: post.date,
+
+                reply: post.reply.map((reply) => ({
+                    id: reply.id,
+                    text: reply.text,
+                    username: reply.username,
+                    date: reply.date,
+                })),
+            },
+        })
+    }catch(e) {
+        return res.status(400).json({
+            message: e.message,
+        })
+    }
+})
+
 // Експортуємо глобальний роутер
-module.exports = router
+module.exports = router   
+
+
+
+
--- front/src/container/post-list/index.js
+++ front/src/container/post-list/index.js
@@ -1,13 +1,55 @@
+import { useState, Fragment } from 'react';
+
 import Title from '../../component/title';
 import Grid from '../../component/grid';
 import Box from '../../component/box';
 
 import PostCreate from '../post-create'
 
+import { Alert, Skeleton, LOAD_STATUS } from '../../component/load';
+
+import { getDate } from '../util/getDate';
+
+import PostItem from '../post-item';
+
+
 export default function Container() {
-    const getData = () => {
-        alert(true)
+    const [status, setStatus] = useState(null);
+    const [messaeg, setMessage] = useState('');
+    const [data, setData] = useState(null);
+
+    const getData = async () => {
+        setStatus(LOAD_STATUS.PROGRESS);
+        try {
+            const res = await fetch ("http://localhost:4000/post-list");
+            const data = await res.json();
+
+            if(res.ok) {
+                setData(convertData(data));
+                setStatus(LOAD_STATUS.SUCCESS);
+            } else {
+                setMessage(data.messaeg);
+                setStatus(LOAD_STATUS.ERROR);
+            }
+        } catch (error) {
+            setMessage(error.messaeg)
+            setStatus(LOAD_STATUS.ERROR);
+        }
     };
+
+    const convertData = (raw) => ({
+        list: raw.list.reverse().map(({id,username,text,date}) =>({
+            id,
+            username,
+            text,
+            date: getDate(date),
+        })),
+        isEmpty: raw.list.length === 0,
+    });
+
+    if(status === null) {
+        getData();
+    }
 
     return(
         <Grid>
@@ -21,6 +63,35 @@ export default function Container() {
                     />
                 </Grid>
             </Box>
+
+            {status === LOAD_STATUS.PROGRESS && (
+                <Fragment>
+                    <Box>
+                        <Skeleton />
+                    </Box>
+                    <Box>
+                        <Skeleton />
+                    </Box>
+                </Fragment>
+            )}
+
+            {status === LOAD_STATUS.ERROR && (
+                <Alert status={status} messaeg={messaeg} />
+            )}
+
+            {status === LOAD_STATUS.SUCCESS && (
+                <Fragment>
+                    {data.isEmpty ? (
+                        <Alert messaeg="Список постiв пустий" />
+                    ) : (
+                        data.list.map((item) => (
+                            <Fragment key={item.id}>
+                               <PostItem {...item}/>
+                                </Fragment>
+                        ))
+                    )}
+                </Fragment>
+            )}
         </Grid>
-    )
+    );
 }
